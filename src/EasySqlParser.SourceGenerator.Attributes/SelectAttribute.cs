fddfcc837a0834b5246660d4500c5df913321b63
Nobuyuki Yoshimura <don_muscle@hotmail.com>
2021-08-31 17:28:21 +0900

    add UseDbSet

--- src/EasySqlParser.SourceGenerator.Attributes/SelectAttribute.cs
+++ src/EasySqlParser.SourceGenerator.Attributes/SelectAttribute.cs
@@ -1,9 +1,13 @@
 ﻿namespace EasySqlParser.SourceGenerator.Attributes
 {
     /// <summary>
-    ///     Attribute for SELECT sql
+    ///     Attribute for SELECT sql.
     /// </summary>
     public class SelectAttribute : MethodAttributeBase
     {
+        /// <summary>
+        ///     Gets or sets whether to map via DbSet.
+        /// </summary>
+        public bool UseDbSet { get; set; }
     }
 }
