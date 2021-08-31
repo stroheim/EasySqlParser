c1b7db82f1657dd57f8994c2abd9ce068fded783
Nobuyuki Yoshimura <don_muscle@hotmail.com>
2021-08-31 17:26:25 +0900

    add new attribute

---
+++ src/EasySqlParser.SourceGenerator.Attributes/MergeAttribute.cs
@@ -0,0 +1,22 @@
+﻿namespace EasySqlParser.SourceGenerator.Attributes
+{
+    /// <summary>
+    ///     Attribute for MERGE sql.
+    /// </summary>
+    public class MergeAttribute : MethodAttributeBase
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="MergeAttribute"/> class.
+        /// </summary>
+        /// <param name="sqlFilePath"></param>
+        public MergeAttribute(string sqlFilePath)
+        {
+            FilePath = sqlFilePath;
+        }
+
+        /// <summary>
+        ///     Gets or sets whether use Microsoft SQL Server.
+        /// </summary>
+        public bool UseSqlServer { get; set; }
+    }
+}
---
+++ src/EasySqlParser.SourceGenerator.Attributes/SoftDeleteAttribute.cs
@@ -0,0 +1,37 @@
+﻿namespace EasySqlParser.SourceGenerator.Attributes
+{
+    /// <summary>
+    ///     Attribute for "Soft" DELETE sql.
+    /// </summary>
+    public class SoftDeleteAttribute : MethodAttributeBase
+    {
+
+        /// <summary>
+        ///     Gets or sets whether the version property is ignored.
+        /// </summary>
+        /// <remarks>
+        ///     Do not include version no in update conditions.<br/>
+        ///     Version no itself is updated with the set value.
+        /// </remarks>
+        public bool IgnoreVersion { get; set; } = false;
+
+        /// <summary>
+        ///     Gets or sets whether SQL NULL columns are excluded from SQL INSERT statements.
+        /// </summary>
+        public bool ExcludeNull { get; set; } = false;
+
+        /// <summary>
+        ///     Gets or sets whether OptimisticLockException is suppressed.
+        /// </summary>
+        /// <remarks>
+        ///     Include version no. in update conditions.<br/>
+        ///     Do not throw OptimisticLockException even if the number of updates is 0.
+        /// </remarks>
+        public bool SuppressOptimisticLockException { get; set; } = false;
+
+        /// <summary>
+        ///     Gets or sets whether to automatically generate SQL statements. 
+        /// </summary>
+        public bool AutoGenerateSql { get; set; } = true;
+    }
+}
